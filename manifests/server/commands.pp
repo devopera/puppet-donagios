6f3a31ea5e57bccbc1f1cf96018d7aa52d090679
cleverlight <github.account@cleveraspaint.com>
2013-10-17 09:23:10 +0100

    added and commented commands

--- manifests/server/commands.pp
+++ manifests/server/commands.pp
@@ -12,8 +12,21 @@ class donagios::server::commands (
 
   # setup nagios commands for default (installed) nagios plugins
   nagios_command { 'check_procs' :
+    # -w warning RANGE
+    # -c critical RANGE
+    # -C command
     # ranges are min:[max]
     command_line => '$USER1$/check_procs -w $ARG1$ -c $ARG2$ -C $ARG3$'
+  }
+
+  nagios_command { 'check_disk_smb' :
+    # -H hostname
+    # -s share
+    # -W workgroup
+    # -a IP address
+    # -u username
+    # -p password
+    command_line => '$USER1$/check_disk_smb -H $ARG1$ -s $ARG2$ -W $ARG3$ -a $ARG4$ -u $ARG5$ -p $ARG6$'
   }
 
 }
