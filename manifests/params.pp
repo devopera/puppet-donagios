63a61743bce75c6d5cfc8ff2b9e6ca68f751318e
cleverlight <github.account@cleveraspaint.com>
2015-01-24 22:01:42 +0000

    made CentOS use nrpe user for nrpe checking, only Ubuntu uses a single nagios user

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,7 +3,7 @@ class donagios::params {
   # Ubuntu uses a single nagios user for nrpe
   case $operatingsystem {
     centos, redhat, fedora: {
-      $user_name_nrpe = 'nagios'
+      $user_name_nrpe = 'nrpe'
     }
     ubuntu, debian: {
       $user_name_nrpe = 'nagios'
